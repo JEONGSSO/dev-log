0fb90008a546205baa62a0597f7f314c674ae97c
jeongsso <rpsha33@naver.com>
2020-08-18 22:04:45 +0900

    gp

--- utils/fp.js
+++ utils/fp.js
@@ -1,66 +1,19 @@
+const L = {};
+const log = console.log;
+
 const curry = (f) => (a, ..._) =>
   _.length ? f(a, ..._) : (..._) => f(a, ..._);
-
-const map = curry((f, iter) => {
-  const arr = [];
-  for (const v of iter) {
-    arr.push(f(v));
-  }
-  return arr;
-});
-
-const filter = curry((f, iter) => {
-  const arr = [];
-  for (const v of iter) {
-    if (f(v)) arr.push(v);
-  }
-  return arr;
-});
-
-// const reduce = (f, acc, iter) => {
-//   if (!iter) {
-//     iter = acc[Symbol.iterator]();
-//     acc = iter.next().value;
-//   }
-
-//   for (const v of iter) {
-//     console.log('ttttttttttttt', iter);
-//     acc = f(acc, v);
-//   }
-//   return acc;
-// };
 
 const reduce = curry((f, iter, acc) => {
   if (acc === undefined) {
     iter = iter[Symbol.iterator]();
     acc = iter.next().value;
   }
-
   for (const v of iter) {
     acc = f(acc, v);
   }
   return acc;
 });
-
-const go = (...args) => reduce((a, f) => f(a), args);
-
-const pipe = (fn, ...fns) => (...as) => go(fn(...as), ...fns);
-
-const range = (num) => {
-  let i = -1;
-  const arr = [];
-  while (++i < num) {
-    arr.push(i);
-  }
-  return arr;
-};
-
-const lRange = function* (num) {
-  let i = -1;
-  while (++i < num) {
-    yield i;
-  }
-};
 
 const take = curry((l, iter) => {
   let res = [];
@@ -71,20 +24,46 @@ const take = curry((l, iter) => {
   return res;
 });
 
-const lMap = curry(function* (f, iter) {
+const takeAll = take(Infinity)
+  
+const go = (...args) => reduce((a, f) => f(a), args);
+
+const pipe = (fn, ...fns) => (...as) => go(fn(...as), ...fns);
+
+L.range = function* (num) {
+  let i = -1;
+  while (++i < num) {
+    yield i;
+  }
+};
+
+L.map = curry(function* (f, iter) {
   for (const a of iter) yield f(a);
 });
 
-const lFilter = curry(function* (f, iter) {
-  for (const a of iter) if (f(a)) yield a;
+L.filter = curry(function* (f, iter) {
+  for (const a of iter) {
+    if(f(a)) yield a;
+  }
 });
 
-const find = (f, iter) => go(
-  iter,
-  filter(f),
-  take(1),
-  ([a]) => a
-)
+const map = curry(pipe(L.map, takeAll));
+
+const filter = curry(pipe(L.filter, takeAll));
+
+const range = (start, stop, step = 1) => {
+  const arr = [];
+  let i = stop ? start : 0;
+  stop = stop || start;
+  while (i < stop) {
+    arr.push(i);
+    i += step;
+  }
+  return arr;
+};
+
+const join = curry((sep = ',', list) => 
+    reduce((pre, val) => pre+sep+val, list))
 
 // export default {
 //   map,
