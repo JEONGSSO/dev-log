9c6a36e0a1a9086de0d48b11255ae43865c92aff
jeongsso <rpsha33@naver.com>
2021-03-23 23:52:20 +0900

    문제

---
+++ js/z_solution.js
@@ -0,0 +1,19 @@
+(() => {
+  let count = 0;
+  const init = 26;
+
+  const solution = (n) => {
+    if (n / 1 === init && count) {
+      return count;
+    }
+
+    const second = n % 10;
+    const first = (n - second) / 10;
+    const sum = first + second > 9 ? (first + second) % 10 : first + second;
+
+    count++;
+    return solution(second + sum.toString());
+  };
+
+  console.log(solution(init > 9 ? init : "0" + init));
+})();
