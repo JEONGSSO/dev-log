1a3f2fe536093d675d4a8f2f05d072ac2f9abe05
jeongsso <rpsha33@naver.com>
2021-12-24 13:54:51 +0900

    split algorithm

--- js/알고리즘/2_삼각형_판별하기.js
+++ js/알고리즘/2_삼각형_판별하기.js
@@ -6,9 +6,3 @@ console.log(solution(13, 33, 17));
 console.log(solution(15, 15, 29));
 
 // 매 2 변의 길이의 합계는 세번째 측면의 길이를 초과한다.
-
-const sol2 = (a) => Math.ceil(a / 12);
-
-console.log(sol2(25));
-console.log(sol2(61));
-console.log(sol2(178));
---
+++ js/알고리즘/3_연필_다스.js
@@ -0,0 +1,5 @@
+const sol2 = (a) => Math.ceil(a / 12);
+
+console.log(sol2(25));
+console.log(sol2(61));
+console.log(sol2(178));
