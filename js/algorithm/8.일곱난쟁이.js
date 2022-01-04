fe5569ee224242e94469f8f1cc3d6b5dabfe2f78
jeongsso <rpsha33@naver.com>
2022-01-04 17:29:26 +0900

    일곱난쟁이

---
+++ js/algorithm/8.일곱난쟁이.js
@@ -0,0 +1,20 @@
+const solution = (arr) => {
+  const remain = arr.reduce((acc, val) => acc + val) - 100;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j && arr[i] + arr[j] === remain) {
+        arr.splice(i, 1);
+        arr.splice(j, 1);
+        return arr;
+      }
+    }
+  }
+};
+
+// 합이 100이어야 함
+// 다 더하고 두 수를뺀값이 100인경우에 그 두 수를 빼면 됨.
+
+const arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+
+console.log(solution(arr));
